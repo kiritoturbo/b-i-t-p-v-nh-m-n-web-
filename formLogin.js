8cb3561e25a5bc30e18e6e7ae3ecb6e82613c25c
kirito <manhtruong@gmail.com>
2022-01-01 13:48:14 +0700

    form login javascript

--- formLogin.js
+++ formLogin.js
@@ -1,10 +1,18 @@
-function click(){
-    var u=document.getElementById("username").value;
-    var p=document.getElementById("password").value;
+function clickbutton(){
+    var u  =document.getElementById("username").value;
+    var p1  =document.getElementById("password").value;
 
-    if(u==' '){
-        document.getElementById("message").style.color="red";
-        document.getElementById("message").innerHTML="cần nhập username";
+    if(u==""){
+        document.getElementById("message").style.color='red';
+        document.getElementById("message").innerHTML='cần nhập username';
         return false;
+    }else if(p1==""){
+        document.getElementById("message").style.color="blue";
+        document.getElementById("message").innerHTML="cần nhập password";
+        return false;
+    }else{
+        document.getElementById("message").style.color="green";
+        document.getElementById("message").innerHTML="bạn login thành công";
     }
+
 }
