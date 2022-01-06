cf8292aa5fd1c2501608b7d55b7113281dabfab0
kirito <manhtruong@gmail.com>
2022-01-06 14:25:12 +0700

    change name

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ const CONFIG = {
     btnYes: 'yêu ơi là iuuu <3',
     btnNo: 'cậu mơ à:))',
     question: 'Nói cho tới lý do vì sao cậu yêu tớ đi <3',
-    btnReply: 'Gửi cho Tuyên',
+    btnReply: 'Gửi cho Trường',
     reply: 'Tại vì cậu đẹp trai hihi <33333',
     mess: 'Tớ biết mà hihi 🥰 ',
     messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
     btnAccept: 'oke nha <3',
-    messLink: 'mhttps://www.facebook.com/messages/t/100065166715627', //link fb
+    messLink: 'https://www.facebook.com/nguyentr25/', //link fb
     bgLink: 'https://c4.wallpaperflare.com/wallpaper/880/299/772/tree-heart-hearts-love-wallpaper-preview.jpg', //link ảnh nền
     iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
     lookMeLink: 'https://i.pinimg.com/originals/c9/e3/ff/c9e3ff99c0c997ec9572ffae6ad837cc.jpg', //link ảnh lookMe
